0ff0651476cad86c9c782bb164f168ddc6d23e68
Sayem Shafayet <ishafayet@gmail.com>
2018-03-18 17:22:28 +0600

    Adding Windows Task Scheduler entry is now functional

--- ui-index.js
+++ ui-index.js
@@ -1,4 +1,48 @@
 
+const shell = require('shelljs');
+
+const runRemoveTaskSchedule = () => {
+  let res = shell.exec('schtasks /Delete /TN "SelfieWatcher" /F')
+  if (res.code !== 0) {
+    let knownError1 = 'The specified task name "SelfieWatcher" does not exist in the system.';
+    let knownError2 = 'The system cannot find the file specified.';
+    let knownError3 = 'Access is denied.';
+    if (res.stderr.indexOf(knownError1) === -1 && res.stderr.indexOf(knownError2) === -1) {
+      if (res.stderr.indexOf(knownError3) !== -1) {
+        let message = "Access is denied. Please run this app as an administrator for this feature to work.";
+        alert(message);
+      } else {
+        let message = 'Error: Deleting task failed. Details: ' + res.stdout + res.stderr;
+        console.log(message);
+        alert(message);
+      }
+    }
+  }
+}
+
+const runImportTaskSchedule = () => {
+  let scheduleFilePath = './WindowsTaskSchedule.xml';
+  let content = fslib.readFileSync(scheduleFilePath, { encoding: 'ucs2' });
+  scheduleFilePath = pathlib.resolve('./WindowsTaskScheduleReady.xml');
+  let executablePath = process.execPath;
+  let sourcePath = process.cwd();
+  content = content.replace(/\$EXE\$/g, executablePath);
+  content = content.replace(/\$CWD\$/g, sourcePath);
+  fslib.writeFileSync(scheduleFilePath, content, { encoding: 'ucs2' });
+  let res = shell.exec(`schtasks /Create /TN "SelfieWatcher" /XML "${scheduleFilePath}"`);
+  if (res.code !== 0) {
+    let knownError1 = 'Access is denied.';
+    if (res.stderr.indexOf(knownError1) !== -1) {
+      let message = "Access is denied. Please run this app as an administrator for this feature to work.";
+      alert(message);
+    } else {
+      let message = 'Error: Deleting task failed. Details: ' + res.stdout + res.stderr;
+      console.log(message);
+      alert(message);
+    }
+  }
+}
+
 let config = null;
 
 const previewCamera = (cbfn) => {
@@ -13,18 +57,24 @@ const previewCamera = (cbfn) => {
         })
     }).then(() => fileController.initialize({ dir: config.dir }))
     .then(() => {
-      return cbfn();
+      return cbfn(null);
     })
     .catch((err) => {
       console.error(err);
-      alert(err.message);
+      // alert(err.message);
       // nw.App.quit();
+      cbfn(err.message);
     });
 }
 
 $(document).ready(() => {
 
-  previewCamera(() => {
+  previewCamera((errorMessage) => {
+
+    if (errorMessage) {
+      errorMessage = 'Error: <br>' + errorMessage;
+      $('#error-message').html(errorMessage);
+    }
 
     $('#save_location').val(config.dir);
 
@@ -42,6 +92,11 @@ $(document).ready(() => {
       }
       config.dir = dir;
       $('#save_location').val(dir);
+    });
+
+    $('#create-task-schedule-button').on('click', () => {
+      runRemoveTaskSchedule();
+      runImportTaskSchedule();
     });
 
   });
